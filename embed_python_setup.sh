e160d3f10c16e0a7605e3ca754cda3399c9a752e
k4sima_0 <44926913+k4sima@users.noreply.github.com>
2022-05-20 13:43:22 +0900

    simple step

---
+++ embed_python_setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+# ./linux_setup.sh <PYTHON_VERSION>
+
+# default 3.9.9
+VERSION=${1:-3.9.9}
+
+# Download
+curl https://www.python.org/ftp/python/$VERSION/python-$VERSION-embed-amd64.zip -o python.zip
+unzip -d python python.zip
+rm -rf python.zip
+
+# Get pip
+curl https://bootstrap.pypa.io/get-pip.py -o python/get-pip.py
+sed -i "s/#import site/import site/g" python/*._pth
+python/python.exe python/get-pip.py
+rm -rf python/get-pip.py
+
+echo -e "\nInstalled Python-$VERSION"
---
+++ test.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hello EmbeddablePython!')
+
+
+if __name__ == '__main__':
+    main()
